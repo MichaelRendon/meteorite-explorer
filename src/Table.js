64a3137af497eaf9a3d824a093613a15fedb1491
MichaelRendon <49463393+MichaelRendon@users.noreply.github.com>
2019-05-19 15:29:57 -0400

    added DataSet.js and mock table for data

---
+++ src/Table.js
@@ -0,0 +1,9 @@
+<p>Name</p>
+				<p>Id</p>
+				<p>Name Type</p>
+				<p>Rec Class</p>
+				<p>Mass (g)</p>
+				<p>Fall</p>
+				<p>Year</p>
+				<p>Lattitude</p>
+				<p>Longitude</p>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Explorer from './Explorer';
+import DataSet from './DataSet';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render(<Explorer />, document.getElementById('root'));
+ReactDOM.render(<DataSet />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+/*DataSet is Meteorite Landing DataSet found at
+ https://data.nasa.gov/Space-Science/Meteorite-Landings/gh4g-9sfh*/
