145ed2267db22daaae2564640f688ab897bfc183
MichaelRendon <49463393+MichaelRendon@users.noreply.github.com>
2019-05-19 22:37:05 -0400

    added Meteorites.js

---
+++ src/Meteorites.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Meteorites = () => {
+	return (
+		<div>
+			<table alt='data table' src='https://data.nasa.gov/resource/gh4g-9sfh.json' />
+		  <div>
+		  <th>Name</th>
+		  <td>Meteorite 1</td>
+		  </div>
+		</div>
+	);
+}
+
+
+
+export default Meteorites;
